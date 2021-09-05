68d01a64e3f83dd17c4525aa549430060878514d
Vanessa Araújo <vanessacaraujo0301@gmail.com>
2021-09-04 22:01:20 -0300

    testes .net 5

---
+++ TopLevelExemplo/ClasseTopLevel.cs
@@ -0,0 +1,5 @@
+//System.Console.WriteLine("Erro");
+public class ClasseSemNamespace
+{
+        public string Propriedade { get => "Classe sem namespace"; }
+}
---
+++ TopLevelExemplo/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using TopLevelExemplo;
+
+Console.WriteLine("Aplicação console sem o método main");
+
+//Mesmo utilizando Top Levels Statement ainda é possivel utilizar o 'args' que se encontra no escopo 
+foreach(var argumento in args)
+{
+    Console.WriteLine($"Argumento: {argumento}");
+}
+
+FuncaoLocal();
+
+//Chamando uma função local, recurso disponível desde o C# 7.0
+void FuncaoLocal()
+{
+    Console.WriteLine("Output da funcao Local");
+}
+
+//Instanciando uma classe e obtendo uma propriedade
+var teste = new ClasseComum();
+Console.WriteLine(teste.Propriedade);
+
+var teste1 = new ClasseTopLevel();
+Console.WriteLine(teste1.Propriedade);
+
+var teste2 = new ClasseSemNamespace();
+Console.WriteLine(teste2.Propriedade);
+ 
+class ClasseTopLevel
+{
+public string Propriedade { get => "Classe Top level"; }
+
+ }
